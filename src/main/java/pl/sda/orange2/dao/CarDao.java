98aa19c98de3111b8dfd72c90c241b6891de93c5
szogunn <mateusz1_boczek1@interia.pl>
2023-03-07 21:02:52 +0100

    [add] - CarDao class added as prototype of methods

---
+++ src/main/java/pl/sda/orange2/dao/CarDao.java
@@ -0,0 +1,29 @@
+package pl.sda.orange2.dao;
+
+import pl.sda.orange2.entity.CarRec;
+
+import java.util.List;
+
+public class CarDao {
+
+    public void save(CarRec carRec){
+
+    }
+
+    public List<CarRec> findAll(){
+
+        return List.of();
+    }
+
+    public CarRec findById(Long id){
+
+        return null;
+    }
+
+    public void deletyById(Long id){
+
+    }
+
+
+
+}
